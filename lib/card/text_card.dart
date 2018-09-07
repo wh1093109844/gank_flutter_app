28630bb0840dc5fe81f36e8005778296dd0e29f9
wanghe <wanghe@zhongan.io>
2018-09-07 18:37:33 +0800

    [1]更新

--- lib/card/text_card.dart
+++ lib/card/text_card.dart
@@ -9,23 +9,47 @@ class TextCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      List<Widget> contentList = [new Expanded(child: new Text(gank.desc))];
+      List<Widget> contentList = [
+          new Expanded(
+                  child: new Column(
+                      children: <Widget>[
+                          new Padding(
+                                  padding: EdgeInsets.only(bottom: 10.0),
+                                  child: new Text(
+                                      gank.who,
+                                      style: new TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15.0,
+                                          wordSpacing: 2.0
+                                      ),
+                                  ),
+                          ),
+                          new Text(
+                              gank.desc,
+                              maxLines: 2,
+                              ),
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                  ),
+      ];
       if (gank.images != null && gank.images?.isNotEmpty) {
-          contentList.add(new Image.network(gank.images[0], width: 120.0, height: 60.0, fit: BoxFit.cover,));
+          contentList.add(new Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: new Image.network(
+                      gank.images[0],
+                      width: 120.0,
+                      height: 60.0,
+                      fit: BoxFit.cover,
+                      ),
+                  ),
+                          );
       }
     return new Card(
-        child: new Column(
-            children: <Widget>[
-                new Row(
-                    children: <Widget>[
-                        new Text(gank.who),
-                    ],
-                ),
-                new Row(
-                    children: contentList,
-                )
-            ],
-        ),);
+        child: new Container(
+            child: new Row(children: contentList,),
+            padding: EdgeInsets.all(10.0),),
+        );
   }
 
 }
